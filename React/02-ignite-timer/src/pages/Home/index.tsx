d8733cda07a5ccfcb5768184e8413733ed1faedd
Leonardo Mendonça <leonardomendonca2001@gmail.com>
2024-02-14 07:56:55 -0300

    a

--- React/02-ignite-timer/src/pages/Home/index.tsx
+++ React/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,4 +1,10 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod'
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from 'date-fns'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -9,37 +15,119 @@ import {
   TaskInput,
 } from "./styles";
 
+// interface FormCycleProps {
+//   task: string
+//   minutesAmount: number
+// }
+
 export function Home() {
+
+  interface Cycle {
+    id: string
+    task: string
+    minutesAmount: number
+    startDate: Date
+  }
+
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Tarefa não informada"),
+    minutesAmount: zod.number().min(1, "Valor minimo: 1").max(60, "Valor máximo: 60")
+  })
+
+  type FormCycleProps = zod.infer<typeof newCycleFormValidationSchema>
+
+  const { register, handleSubmit, watch, reset } = useForm<FormCycleProps>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
+
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+      }, 1000)
+    }
+  }, [activeCycle]);
+
+  function handleCreateNewCycle(data: FormCycleProps) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
+    }
+
+    setCycles(state => [...state, newCycle])
+
+    setActiveCycleId(id)
+
+    reset()
+  }
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSecounds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSecounds / 60)
+  const secondAmount = currentSecounds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const second = String(secondAmount).padStart(2, '0')
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             type="text"
+            list="task-suggestions"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">Durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={2}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{second[0]}</span>
+          <span>{second[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Começar
         </StartCountdownButton>
--- React/02-ignite-timer/src/pages/Home/styles.ts
+++ React/02-ignite-timer/src/pages/Home/styles.ts
@@ -39,12 +39,21 @@ const BaseInput = styled.input`
     color: ${ props => props.theme['gray-100']};
 
     &::placeholder {
-        
+        color: ${ props => props.theme['gray-500'] };
+    }
+
+    &:focus{
+        box-shadow: none;
+        border-color: ${ props => props.theme['green-500'] };
     }
 `
 
 export const TaskInput = styled(BaseInput)`
     flex: 1;
+
+    &::-webkit-calendar-picker-indicator{
+        display: none !important;
+    }
 `;
 
 export const MinutesAmountInput = styled(BaseInput)`
